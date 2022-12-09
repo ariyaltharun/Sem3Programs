1ed9061f4c620cd4b5b57c0f7db4070227910e9c
ariyaltharun <ariyaltharun@gmail.com>
2022-12-09 01:05:37 +0000

    Buggy selection sort using macro and DMA v1.0.0

---
+++ selectionSortDSProgram.c
@@ -0,0 +1,41 @@
+/* Program to perform selection sort */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SWAP(x, y) (x = x+y, y = x-y, x = x-y)
+
+int main() {
+	int n, *arr, i, j;
+
+	// Dynamically allocating the size of an array
+	printf("[ QUESTION ] Enter the size of an array : ");
+	scanf("%d", &n);
+	arr = malloc(n*sizeof(int));
+
+
+	// Accept the elements from the keyboard
+	printf("[ QUESTION ] Enter the element of an array\n");
+	for (i=0; i<n; i++)
+		scanf("%d", arr[i]);   /* Segmentation fault error */
+
+
+	// Logic Implementation (Main Part)
+	for (i=0; i<n; i++) {
+		int index = arr[i];
+		for (int j=i; j<n; j++)
+			if (arr[index] < arr[j])
+				index = j, arr[index] = arr[j];
+				
+		SWAP(arr[i], arr[j]);
+	}
+
+
+	// Printing the sorted array
+	printf("[ INFO ] Sorted array : ");
+	for (i=0; i<n; i++)
+		printf("%d ", arr[i]);
+
+	free(arr);
+	return 0;
+}
