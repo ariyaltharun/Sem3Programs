8f6bf5f8e55369eb41db747716c6a51f00488c88
ariyaltharun <107637700+ariyaltharun@users.noreply.github.com>
2022-12-11 18:12:05 +0000

    Literally Modified Everything

--- VehicleInheritence.java
+++ VehicleInheritence.java
@@ -1,76 +1,110 @@
 import java.util.Scanner;
 
+// a) Create a abstract class called Vehicle
 abstract class Vehicle {
-    String hashelmet;
+    String hasHelmet;
     int yearOfManufacture;
-
-    Vehicle(String hashelmet, int yearOfManufacture) {
-        this.hashelmet = hashelmet;
-        this.yearOfManufacture = yearOfManufacture;
-    }
 
     abstract void getData();
 
     abstract void putData();
+
+    Vehicle(String hasHelmet) {
+        this.hasHelmet = hasHelmet;
+    }
+
+    Vehicle(int yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
 }
 
 
+// b) Create two derived classes called "TwoWheeler" and "FourWheeler"
 class TwoWheeler extends Vehicle {
-    Scanner scan = new Scanner(System.in);
-
     String Brand, EngineType, Color;
-    float Cost;
+    double Cost;
 
-    TwoWheeler(String  hashel, int year) {
-        super(hashel, year);
-    }
-    @Override
-    void getData() {
-        System.out.println("Enter the Brand name, Cost, EngineType, Color");
-        Brand = scan.nextLine();
-        Cost = scan.nextFloat();
-        EngineType = scan.nextLine();
-        Color = scan.nextLine();
+    TwoWheeler(String hasHelmet) {
+        super(hasHelmet);
     }
 
-    @Override
+
     void putData() {
-        System.out.println("Details of Two Wheeler");
-        System.out.println(super.hashelmet);
-        System.out.println(super.yearOfManufacture);
-        System.out.println(Brand);
-        System.out.println(Cost);
-        System.out.println(EngineType);
-        System.out.println(Color);
+        this.Brand = "Hero";
+        this.Cost = 1.5;
+        this.EngineType = "2 Stroke";
+        this.Color = "Sports Red";
+    }
+
+    void getData() {
+        System.out.println("Two Wheeler Vehicle details");
+        System.out.println("Brand = " + Brand);
+        System.out.println("Cost = " + Cost);
+        System.out.println("Engine Type = " + EngineType);
+        System.out.println("Color = " + Color);
+        System.out.println("Has Helmet? " + hasHelmet);
     }
 }
-
 
 
 final class FourWheeler extends Vehicle {
-    FourWheeler(String h, int yr) {
-        super(h, yr);
+    Scanner scan = new Scanner(System.in);
+    String Brand, EngineType, Color;
+    double Cost;
+
+    FourWheeler(int yearOfManufacture) {
+        super(yearOfManufacture);
     }
-    @Override
+
+
     void putData() {
-
+        System.out.println("\nEnter the Details of Four Wheeler Vehicle");
+        System.out.print("Enter the brand : ");
+        Brand = scan.next();
+        System.out.print("Enter the cost : ");
+        Cost = scan.nextDouble();
+        System.out.print("Enter the Engine Type : ");
+        EngineType = scan.nextLine();
+        System.out.print("Enter the Color : ");
+        Color = scan.next();
     }
 
-    @Override
     void getData() {
-
+        System.out.println("\nFour Wheeler Details");
+        System.out.println("Year of Manufacture = " + yearOfManufacture);
+        System.out.println("Brand = " + Brand);
+        System.out.println("Cost = " + Cost);
+        System.out.println("Engine Type = " + EngineType);
+        System.out.println("Color = " + Color);
     }
 }
-public class VehicleInheritence {
+
+
+
+
+
+class MyTwoWheeler extends TwoWheeler {
+    String ownerName;
+
+    MyTwoWheeler() {
+        super("Yes");
+        super.putData();
+        this.ownerName = "Witch";
+    }
+
+}
+
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyTwoWheeler twoWheeler = new MyTwoWheeler();
+        FourWheeler four = new FourWheeler(2022);
 
-        Vehicle v = new Vehicle();
+        System.out.println("Two Wheeler owner name : " + twoWheeler.ownerName);
+        twoWheeler.getData();
 
-        TwoWheeler bike = new TwoWheeler("Yes", 2030);
+        four.putData();
+        four.getData();
 
-        bike.getData();
-
-        bike.putData();
     }
 }
